db99ae820bd650b4bb2562cffe925693402e2c04
Cyan Kneelawk <kneelawk@gmail.com>
2024-11-16 08:10:22 -0800

    Make WeakKeys equal their wrapped objects

--- core/xplat/src/main/java/com/kneelawk/commonevents/impl/event/WeakKey.java
+++ core/xplat/src/main/java/com/kneelawk/commonevents/impl/event/WeakKey.java
@@ -2,7 +2,7 @@ package com.kneelawk.commonevents.impl.event;
 
 import java.lang.ref.WeakReference;
 
-public class WeakKey {
+public final class WeakKey {
     private final WeakReference<Object> ref;
     private final int hashCode;
 
@@ -13,17 +13,20 @@ public class WeakKey {
 
     @Override
     public boolean equals(Object o) {
+        // hash-code/equals magic that makes it so that a weak-key hash-code/equals the same as the object it's holding
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        WeakKey weakKey = (WeakKey) o;
+        if (o == null) return false;
 
         Object o1 = ref.get();
         // null means we lost this ref, so we compare hashCodes because that's all we have left
-        if (o1 == null) return hashCode == weakKey.hashCode;
+        if (o1 == null) return o instanceof WeakKey weakKey && hashCode == weakKey.hashCode;
 
-        Object o2 = weakKey.ref.get();
-        return o1.equals(o2);
+        if (o instanceof WeakKey weakKey) {
+            Object o2 = weakKey.ref.get();
+            return o1.equals(o2);
+        } else {
+            return o1.equals(o);
+        }
     }
 
     @Override
