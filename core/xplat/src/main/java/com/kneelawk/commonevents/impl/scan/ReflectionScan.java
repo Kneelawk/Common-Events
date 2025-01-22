d02926f8d200247e114ccc62604f7db4920e6986
Cyan Kneelawk <kneelawk@gmail.com>
2025-01-22 12:16:22 -0800

    Cache scan results

--- core/xplat/src/main/java/com/kneelawk/commonevents/impl/scan/ReflectionScan.java
+++ core/xplat/src/main/java/com/kneelawk/commonevents/impl/scan/ReflectionScan.java
@@ -2,8 +2,12 @@ package com.kneelawk.commonevents.impl.scan;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.ImmutableList;
 
 import net.minecraft.resources.ResourceLocation;
 
@@ -11,40 +15,56 @@ import com.kneelawk.commonevents.api.EventKey;
 import com.kneelawk.commonevents.api.Listen;
 
 public record ReflectionScan(EventKey key, ResourceLocation phase, Method method) {
+    private static final Cache<Class<?>, List<ReflectionScan>> instanceMethods =
+        CacheBuilder.newBuilder().concurrencyLevel(8).build();
+    private static final Cache<Class<?>, List<ReflectionScan>> staticMethods =
+        CacheBuilder.newBuilder().concurrencyLevel(8).build();
+
     public static List<ReflectionScan> scanInstance(Object listeners) {
-        List<ReflectionScan> results = new ArrayList<>();
-
         Class<?> clazz = listeners.getClass();
-        for (Method m : clazz.getMethods()) {
-            if (Modifier.isPublic(m.getModifiers())) {
-                Listen l = m.getAnnotation(Listen.class);
-                if (l != null) {
-                    results.add(
-                        new ReflectionScan(EventKey.fromClass(l.value(), l.qualifier()),
-                            ResourceLocation.parse(l.phase()),
-                            m));
-                }
-            }
-        }
 
-        return results;
+        try {
+            return instanceMethods.get(clazz, () -> {
+                ImmutableList.Builder<ReflectionScan> builder = ImmutableList.builder();
+
+                for (Method m : clazz.getMethods()) {
+                    if (Modifier.isPublic(m.getModifiers())) {
+                        Listen l = m.getAnnotation(Listen.class);
+                        if (l != null) {
+                            builder.add(new ReflectionScan(EventKey.fromClass(l.value(), l.qualifier()),
+                                ResourceLocation.parse(l.phase()), m));
+                        }
+                    }
+                }
+
+                return builder.build();
+            });
+        } catch (ExecutionException e) {
+            // should never happen
+            throw new RuntimeException(e);
+        }
     }
 
     public static List<ReflectionScan> scanStatic(Class<?> clazz) {
-        List<ReflectionScan> results = new ArrayList<>();
+        try {
+            return staticMethods.get(clazz, () -> {
+                ImmutableList.Builder<ReflectionScan> builder = ImmutableList.builder();
 
-        for (Method m : clazz.getDeclaredMethods()) {
-            if (Modifier.isPublic(m.getModifiers()) && Modifier.isStatic(m.getModifiers())) {
-                Listen l = m.getAnnotation(Listen.class);
-                if (l != null) {
-                    results.add(
-                        new ReflectionScan(EventKey.fromClass(l.value(), l.qualifier()),
-                            ResourceLocation.parse(l.phase()),
-                            m));
+                for (Method m : clazz.getDeclaredMethods()) {
+                    if (Modifier.isPublic(m.getModifiers()) && Modifier.isStatic(m.getModifiers())) {
+                        Listen l = m.getAnnotation(Listen.class);
+                        if (l != null) {
+                            builder.add(new ReflectionScan(EventKey.fromClass(l.value(), l.qualifier()),
+                                ResourceLocation.parse(l.phase()), m));
+                        }
+                    }
                 }
-            }
-        }
 
-        return results;
+                return builder.build();
+            });
+        } catch (ExecutionException e) {
+            // should never happen
+            throw new RuntimeException(e);
+        }
     }
 }
